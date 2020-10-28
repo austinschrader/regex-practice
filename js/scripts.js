25e454680f31c2f04f329aeb945f5bad25d15942
Austin Schrader <austin.jschrader@gmail.com>
2020-10-28 13:43:26 -0700

    All buttons now working
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,44 @@ $(document).ready(function () {
 		$('.output1').text(regexFirst);
 
 		// Flag Functionality
-		let flagInput = $('input#flagInput').val();
-		let flagRegex = $('input#flagRegex').val();
-		let regInstance = new RegExp(flagRegex, 'gi');
-		let regValue = flagInput.replace(regInstance, 'x');
-		$('.flagOutput').text(regValue);
+		let flagsInput = $('input#flagsInput').val();
+		let flagsRegex = $('input#flagsRegex').val();
+		let flagsRegInstance = new RegExp(flagsRegex, 'i');
+		let flagsRegValue = flagsInput.replace(flagsRegInstance, 'x');
+		$('.flagsOutput').text(flagsRegValue);
+
+		// Pattern Functionality
+		let patternsInput = $('input#patternsInput').val();
+		let patternsRegex = $('input#patternsRegex').val();
+		let patternsRegInstance = new RegExp(patternsRegex, 'i');
+		let patternsRegValue = patternsInput.replace(patternsRegInstance, 'x');
+		$('.patternsOutput').text(patternsRegValue);
+
+		// Groups Functionality
+		let groupsInput = $('input#groupsInput').val();
+		let groupsRegex = $('input#groupsRegex').val();
+		let groupsRegInstance = new RegExp(groupsRegex, 'i');
+		let groupsRegValue = groupsInput.replace(groupsRegInstance, 'x');
+		$('.groupsOutput').text(groupsRegValue);
+
+		// Characters Functionality
+		let charactersInput = $('input#charactersInput').val();
+		let charactersRegex = $('input#charactersRegex').val();
+		let charactersRegInstance = new RegExp(charactersRegex, 'i');
+		let charactersRegValue = charactersInput.replace(
+			charactersRegInstance,
+			'x'
+		);
+		$('.charactersOutput').text(charactersRegValue);
+
+		// Quantifier Functionality
+		let quantifiersInput = $('input#quantifiersInput').val();
+		let quantifiersRegex = $('input#quantifiersRegex').val();
+		let quantifiersRegInstance = new RegExp(quantifiersRegex, 'i');
+		let quantifiersRegValue = quantifiersInput.replace(
+			quantifiersRegInstance,
+			'x'
+		);
+		$('.quantifiersOutput').text(quantifiersRegValue);
 	});
 });
