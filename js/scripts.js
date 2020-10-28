d788fc3ae81806ab29888e4eccd8926e542abf3d
Austin Schrader <austin.jschrader@gmail.com>
2020-10-28 11:53:51 -0700

    Add the logic for the first regex
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+	$('#regex-form').submit(function (event) {
+		event.preventDefault();
+
+		let first = $('input#basic1').val();
+    let firstRegInput = $('input#basic2').val();
+    let firstReg = new RegExp(firstRegInput, "x")
+		let regexFirst = first.replace(firstReg);
+		console.log(regexFirst);
+		$('.output1').text(regexFirst);
+	});
+});
