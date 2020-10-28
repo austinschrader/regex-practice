5147e2ff6ceb0fc204170d160f3e8034c6478299
Austin Schrader <austin.jschrader@gmail.com>
2020-10-28 13:17:46 -0700

    Add functionality for the Flags example
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,18 @@ $(document).ready(function () {
 	$('#regex-form').submit(function (event) {
 		event.preventDefault();
 
+		// Basic Functionality
 		let first = $('input#basic1').val();
 		let firstRegInput = $('input#basic2').val();
-		let firstReg = new RegExp(firstRegInput);
+		let firstReg = new RegExp(firstRegInput, 'gi');
 		let regexFirst = first.replace(firstReg, 'x');
-		console.log(firstReg);
 		$('.output1').text(regexFirst);
+
+		// Flag Functionality
+		let flagInput = $('input#flagInput').val();
+		let flagRegex = $('input#flagRegex').val();
+		let regInstance = new RegExp(flagRegex, 'gi');
+		let regValue = flagInput.replace(regInstance, 'x');
+		$('flagOutput').text(regValue);
 	});
 });
